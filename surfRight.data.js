b7e1ae140bb8a96991f75e269d31b9723a0cb7c3
nvcnvn <nguyen@open-vn.org>
2013-09-20 22:16:49 +0700

    Change Usage struct

--- surfRight.data.js
+++ surfRight.data.js
@@ -1,31 +1,73 @@
 /**
  * @constructor
+ * @param {Date} the created time
+ * @struct
+ */
+function Duration(begin) {
+	this.begin = {
+		hour: begin.getUTCHours(),
+		minute: begin.getUTCMinutes()
+	};
+}
+
+Duration.prototype.End = function(end) {
+	this.end = {
+		hour: end.getUTCHours(),
+		minute: end.getUTCMinutes()
+	};
+};
+
+/**
+ * @return {boolean}
+ */
+Duration.prototype.Closed = function() {
+	if(typeof this.end != 'undefined') {
+		return true;
+	}
+
+	return false;
+};
+
+/**
+ * @return {int}
+ */
+Duration.prototype.Sum = function() {
+	if(typeof this.end != 'undefined') {
+		var begin = this.begin.hour*60 + this.begin.minute;
+		var end = this.end.hour*60 + this.end.minute;
+		return end - begin;
+	}
+
+	return 0;
+};
+
+/**
+ * @constructor
  * @param {string} domain of the website
  * @struct
  */
 function Usage(domain) {
-	this.domain = domain;
 	var current = new Date();
-	this.start = {
-		hour: current.getHours(),
-		minute: current.getMinutes()
-	};
 
-	this.end = {
-		hour: 0,
-		minute: 0
-	}
+	this.id = current.UTC();
+	this.domain = domain;
+	this.sum = 0;
+	this.records = [new Duration(current)];
 }
 
 /**
  * @return {Usage}
  */
-Usage.prototype.End = function() {
+Usage.prototype.Record = function() {
 	var current = new Date();
-	this.end = {
-		hour: current.getHours(),
-		minute: current.getMinutes()
-	};
+	var lastRecord = this.records[this.records.length-1];
+
+	if(lastRecord.Closed()) {
+		this.records.push(new Duration(current));
+	}else{
+		lastRecord.End(current);
+		this.sum += lastRecord.Sum();
+	}
 
 	return this;
 };
--- surfRight.js
+++ surfRight.js
@@ -1,3 +1,29 @@
-chrome.webRequest.onCompleted.addListener(function(details){
-	console.log(details);
-},{urls: ["*://*/*"]});
+/**
+ * @constructor
+ * @struct
+ */
+function SurfRight(){
+	this.openingDomains = new Array();
+}
+
+SurfRight.prototype.tabUpdated = function(tabId, changeInfo, tab){
+	var url = $('<a></a>', {
+		href: tab.url
+	});
+	var protocol = url.prop('protocol');
+	var domain = url.prop('host');
+	url.remove();
+
+	if(protocol == "http:" || protocol == "https:") {
+		if(this.openingDomains.indexOf(domain) == -1){
+			this.openingDomains.push(domain);
+		}
+	}
+}
+
+
+var surf = new SurfRight();
+// listening to event
+chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab){
+	surf.tabUpdated(tabId, changeInfo, tab);
+});
