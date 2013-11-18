133ad0f0dd077394709044eb2447b174a0305b22
Cao Nguyen <nguyen@open-vn.org>
2013-11-18 08:36:59 +0700

    add pipe chart to statistics

--- extension/js/statistics.js
+++ extension/js/statistics.js
@@ -2,6 +2,7 @@ function StatisticManager() {
 	var self = this;
 
 	this.cTop = Raphael("cTop");
+	this.cMiddle = Raphael("cMiddle");
 	var fin = function () {
 	    this.flag = self.cTop.popup(this.bar.x, this.bar.y, milisecondToString(this.bar.value) || "0").insertBefore(this);
 	};
@@ -16,15 +17,52 @@ function StatisticManager() {
 		self.LoadData("all", function(d){
 			var n = (10 <= d.data.length)?10:d.data.length;
 			var datas = [];
-			var labels = []
+			var labels = [];
+
+			var pipeData = [];
+			var pipeOther = d.total;
+			var pipeLegend = [];
+			var pipeHref = []
+
 			for(var i = 0; i < n; i++) {
+				pipeOther -= d.data[i].sum;
+				pipeData.push(d.data[i].sum);
+				pipeHref.push('http://'+d.data[i].hostname);
+				pipeLegend.push('%%.%% '+d.data[i].hostname);
 				datas.push([d.data[i].sum]);
 				labels.push([d.data[i].hostname]);
 			}
 
+			pipeLegend.push('%%.%% Others');
+			pipeData.push(pipeOther);
+
 			self.cTop.hbarchart(10, 10, (screen.availWidth/100)*90, 220, datas)
 			.hover(fin, fout)
 			.label(labels);
+
+			var pipe = self.cMiddle.piechart(150, 110, 100, pipeData, {
+				legend: pipeLegend,
+				sort: false,
+				//legendpos: "west",
+				href: pipeHref
+			}).hover(function () {
+                this.sector.stop();
+                this.sector.scale(1.1, 1.1, this.cx, this.cy);
+
+                if (this.label) {
+                    this.label[0].stop();
+                    this.label[0].attr({ r: 7.5 });
+                    this.label[1].attr({ "font-weight": 800 });
+                }
+            }, function () {
+                this.sector.animate({ transform: 's1 1 ' + this.cx + ' ' + this.cy }, 500, "bounce");
+
+                if (this.label) {
+                    this.label[0].animate({ r: 5 }, 500, "bounce");
+                    this.label[1].attr({ "font-weight": 400 });
+                }
+            });
+            console.log(pipe);
 		})
 	})
 	.fail(function() {
